0cd066a8844d39441a4491ab6d83dc7de871e1ba
tomho3203 <tom.q.ho-1@ou.edu>
2024-01-24 12:57:06 -0600

    Initial commit

---
+++ R/myquad.R
@@ -0,0 +1,13 @@
+#' A quadratic function
+#'
+#' @param x a numeric vector
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @examples
+#' myquad(x = 1:10)
+myquad <- function(x){
+  x^2 - 5*x + 6
+
+}
