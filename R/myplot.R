848ec168790d161b1b382fbc4b52033ac2928ad7
tomho3203 <tom.q.ho-1@ou.edu>
2024-02-16 21:39:51 -0600

    added myplot and mybin

---
+++ R/myplot.R
@@ -0,0 +1,12 @@
+#' Myplot function
+#'
+#' @param x numeric vector
+#'
+#' @return numeric vector
+#' @export
+#'
+#' @examples
+#' myplot(3) = 5.022557
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
